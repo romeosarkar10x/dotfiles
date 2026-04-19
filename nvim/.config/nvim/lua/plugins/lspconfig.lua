6fed368e6cf8d296435fb2fb8dcac09ef386389a
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-04-19 21:56:46 +0530

    Added config fn; Enabled lsp for lua

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,26 @@
 return {
-    "neovim/nvim-lspconfig"
+    "neovim/nvim-lspconfig",
+    priority = 1000,
+    lazy = false,
+    dependencies = { "mason-org/mason.nvim" },
+    config = function ()
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = { version = "LuaJIT" },
+                    diagnostics = { globals = { "vim" } },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false
+                    },
+                    telemetry = { enable = false },
+                    hint = { enable = true },
+                    completion = { callSnippet = "Replace" },
+                }
+            }
+        })
+
+        vim.lsp.enable("lua_ls")
+    end
 }
+
