de88d8823259f8ad7538ca290fde6b689d3782d1
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-05-17 17:08:18 +0530

    Added nvim_jdtls for improved java lsp support

---
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -0,0 +1,56 @@
+local jdtls = require("jdtls")
+
+local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+local launcher = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+local config_dir = jdtls_path .. "/config_linux" -- use config_mac on macOS
+
+-- workspace folder per project
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
+
+local config = {
+    cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens", "java.base/java.util=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "-jar", launcher,
+        "-configuration", config_dir,
+        "-data", workspace_dir,
+    },
+
+    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+
+    settings = {
+        java = {
+            signatureHelp = { enabled = true },
+            completion = {
+                favoriteStaticMembers = {},
+            },
+        },
+    },
+
+    -- hook up your existing keymaps/capabilities here
+    on_attach = function(client, bufnr)
+        -- example keymaps
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>oi", jdtls.organize_imports, opts)
+    end,
+}
+
+print("launcher: " .. launcher)
+print("config_dir: " .. config_dir)
+print("root_dir: " .. (config.root_dir or "NIL"))
+print("workspace_dir: " .. workspace_dir)
+
+jdtls.start_or_attach(config)
