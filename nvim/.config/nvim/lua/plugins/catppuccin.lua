0c759d847f2a3ce56aff791e5fb094fc49a2362c
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-14 13:05:31 +0530

    Removed setting theme here. Will set in init.lua

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,5 @@
 return {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
-    config = function()
-        local catppuccin = require("catppuccin")
-        vim.cmd.colorscheme("catppuccin-mocha")
-    end
+    priority = 1000
 }
