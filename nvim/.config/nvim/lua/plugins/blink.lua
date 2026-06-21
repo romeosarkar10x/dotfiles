cb3acbe38ed1a094fbb89873306d0b161f0d8c77
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-21 23:49:51 +0530

    Enable signature_help for functions

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -52,7 +52,9 @@ return {
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
         --
         -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+
+        signature = { enabled = true }
     },
     opts_extend = { "sources.default" }
 }
