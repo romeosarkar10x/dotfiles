779c0094004510870d833b1a4615effcaf982e21
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-08-23 18:32:31 +0530

    Show line numbers in neo-tree

--- nvim/.config/nvim/lua/plugins/neo_tree.lua
+++ nvim/.config/nvim/lua/plugins/neo_tree.lua
@@ -15,6 +15,15 @@ return {
                 hide_dotfiles = false,
                 hide_gitignored = false
             }
+        },
+        event_handlers = {
+            {
+                event = "neo_tree_buffer_enter",
+                handler = function ()
+                    vim.opt_local.number = true
+                    vim.opt_local.relativenumber = true
+                end
+            }
         }
     }
 }
