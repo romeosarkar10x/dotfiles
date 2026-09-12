480f990ce9fa972860a82d3b68d01957f2023cc5
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-09-12 21:03:43 +0530

    Added .project file used in JDT (eclipse) project setups

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -25,7 +25,7 @@ local config = {
         "-data", workspace_dir,
     },
 
-    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+    root_dir = require("jdtls.setup").find_root({ ".project", ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
 
     settings = {
         java = {
