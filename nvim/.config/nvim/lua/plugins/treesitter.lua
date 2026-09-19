c0aa4d7ab0aeecb5e715d53437930bc144035207
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-09-19 13:02:47 +0530

    Removed unsupported language git_commit

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
         require("nvim-treesitter").setup()
         require("nvim-treesitter").install({
             "bash", "astro", "c", "dockerfile",
-            "git_commit", "git_config", "git_rebase", "gitattributes", "gitignore",
+            "git_config", "git_rebase", "gitattributes", "gitignore",
             "html", "java", "javascript", "json", "lua", "python", "scss",
             "typescript", "tsx", "xml", "yaml"
         })
